6806e91ef07f2d2b3ddcebf5a3d08c652f3ff7e1
Jadson Morais <morais.jadsonluiz@gmail.com>
2021-02-13 00:10:04 -0300

    Begining

---
+++ src/test/java/com/jadsonmorais/demo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadsonmorais.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
