6928dea4ce6264dcd74f9a99348ad890b1f4713a
Jadson Morais <morais.jadsonluiz@gmail.com>
2021-02-15 19:34:40 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/jadsonmorais/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jadsonmorais.demo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jadsonmorais.demo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {}
