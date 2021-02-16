548e2e6568945e419721d38aa2a5af35f17eeeef
Jadson Morais <morais.jadsonluiz@gmail.com>
2021-02-16 17:59:27 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/jadsonmorais/demo/config/Instantiation.java
+++ src/main/java/com/jadsonmorais/demo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jadsonmorais.demo.domain.Post;
 import com.jadsonmorais.demo.domain.User;
+import com.jadsonmorais.demo.dto.AuthorDTO;
 import com.jadsonmorais.demo.repository.PostRepository;
 import com.jadsonmorais.demo.repository.UserRepository;
 
@@ -34,11 +35,12 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-		Post post1 = new Post(null, sdf.parse("21/03/21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("22/04/21"), "É isso ai!", "Acordei feliz!", maria);
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("22/04/21"), "É isso ai!", "Acordei feliz!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
