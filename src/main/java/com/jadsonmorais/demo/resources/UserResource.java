c0b32b1313d9fea58cf071c45ac84f01861ed324
Jadson Morais <morais.jadsonluiz@gmail.com>
2021-02-15 21:54:43 -0300

    Inserção de usuário com POST

--- src/main/java/com/jadsonmorais/demo/resources/UserResource.java
+++ src/main/java/com/jadsonmorais/demo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.jadsonmorais.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jadsonmorais.demo.domain.User;
 import com.jadsonmorais.demo.dto.UserDTO;
@@ -22,7 +26,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	
+	//@RequestMapping(method=RequestMethod.GET) <-> Equivale a GetMapping nessa situação;
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
@@ -34,5 +38,14 @@ public class UserResource {
 	public ResponseEntity <UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	//@RequestMapping(method=RequestMethod.POST)
+	@PostMapping
+	public ResponseEntity <Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
