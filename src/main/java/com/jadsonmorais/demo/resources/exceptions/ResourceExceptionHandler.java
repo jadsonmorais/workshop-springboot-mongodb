b6aba6174311c9f9afd6ae68f9b2fb1d5f46e6f2
Jadson Morais <morais.jadsonluiz@gmail.com>
2021-02-15 21:32:44 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/jadsonmorais/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.jadsonmorais.demo.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jadsonmorais.demo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não econtrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);		 
+	}
+}
--- src/main/java/com/jadsonmorais/demo/services/UserService.java
+++ src/main/java/com/jadsonmorais/demo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.jadsonmorais.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jadsonmorais.demo.domain.User;
 import com.jadsonmorais.demo.repository.UserRepository;
+import com.jadsonmorais.demo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
